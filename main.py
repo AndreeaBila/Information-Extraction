66bcf1b23ca90e6cb4df000fecf405f5cdce6234
Andreea Bila <andreeabila@yahoo.com>
2018-12-03 19:48:16 +0000

    tokenization, tagging

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import nltk
+import numpy
 
 from os import listdir
 from os.path import isfile, join
+from nltk.tokenize import word_tokenize
+from nltk import ne_chunk
 
 # Declarations of hash map for files and tag details
 mapFiles    = {}
@@ -25,11 +28,19 @@ def readContents():
         # Map file content by file name
         mapFiles[fileName] = fileContent
 
-    for content in mapFiles:
-        print(mapFiles[content])
+    # for content in mapFiles:
+    text = mapFiles['wsj_0001.txt']
+
+    tokens = nltk.word_tokenize(text)
+    print(tokens)
+    print(nltk.pos_tag(tokens))
+    print(ne_chunk(nltk.pos_tag(tokens), binary=False))
+    # from nltk.tag.stanford import POSTagger
+    # stan_tagger = POSTagger('models/english-bidirectional-distdim.tagger', 'standford-postagger.jar')
+    #
+    # stan_tagger.tag(tokens)
 
 # if __name__ == '__main__':
 #     print("Hello world!")
-
 
 readContents()
