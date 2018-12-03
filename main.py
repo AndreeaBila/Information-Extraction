52734cd52b7352e5a7524a2dd0cdc28f454f078f
Andreea Bila <andreeabila@yahoo.com>
2018-12-03 18:55:19 +0000

    data

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 import nltk
 
-if __name__ == '__main__':
-    print("Hello world!")
+from os import listdir
+from os.path import isfile, join
+
+# Declarations of hash map for files and tag details
+mapFiles    = {}
+mapHeaders  = {}
+mapContent  = {}
+mapTags     = {}
+
+def readContents():
+    myPath = "wsj_untagged/";
+    onlyfiles = [f for f in listdir(myPath) if isfile(join(myPath, f))]
+
+    # corpus_root = '/Users/andre/nltk_data/corpora/wsj_untagged/'
+
+    # Read each file
+    for fileName in onlyfiles:
+        # Construct file name and read the file
+        filePath = myPath + fileName
+        file = open(filePath, "r")
+        fileContent = file.read()
+
+        # Map file content by file name
+        mapFiles[fileName] = fileContent
+
+    for content in mapFiles:
+        print(mapFiles[content])
+
+# if __name__ == '__main__':
+#     print("Hello world!")
+
+
+readContents()
